95caf49390efc4b79c89a167b646365d4a9d6885
Isaac Hung <manhei5001@gmail.com>
2021-10-09 10:09:10 +0800

    updated many things

--- shell/config.fish
+++ shell/config.fish
@@ -1,24 +1,25 @@
-alias ls="exa"
-alias la="exa -la"
+#!/bin/sh
+alias ls="exa --icons --git-ignore"
+alias la="exa -la --icons --git-ignore"
 alias cat="bat"
 alias fd="fd -H"
 alias tree="tree -C"
+alias gs="git st"
+alias clear="clear && colorscript -r"
+alias tree="exa -T"
 
 alias fishrc="nvim ~/.config/fish/config.fish"
-alias refish="source ~/.config/fish/config.fish"
-
-alias xmonadrc="nvim ~/.xmonad/xmonad.hs"
-alias xmobarrc="nvim ~/.xmobarrc"
 alias picomrc="nvim ~/.config/picom/picom.conf"
+alias refish="source ~/.config/fish/config.fish"
 
 alias sedit="EDITOR=nvim sudoedit"
 alias mkdirs="mkdir -p"
 
-alias fm="xplr"
-
-# Startup
 export SUDO_EDITOR=nvim
-clear
-#pfetch
-colorscript -r
-starship init fish | source
+set fish_greeting
+set EDITOR nvim
+set PATH /usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl/:/home/toxicfs/.local/bin:/home/toxicfs/.cargo/bin/
+if status is-interactive
+    clear
+    starship init fish | source
+end
