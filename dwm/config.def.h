fe205017864dea041601bf1e9bf25db1df26a690
Isaac Hung <manhei5001@gmail.com>
2021-11-12 16:04:18 +0800

    revert

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=7" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=7";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
 /* One Dark */
 static const char col_gray1[]       = "#282c34";
 static const char col_gray2[]       = "#353b45";
