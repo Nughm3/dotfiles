33a93e9888bbee36a399aa136d088be2cb194734
Isaac Hung <manhei5001@gmail.com>
2022-01-27 14:04:05 +0800

    commit before changing OS

--- shell/config.fish
+++ shell/config.fish
@@ -1,31 +1,27 @@
-#!/bin/sh
-alias ls="exa --icons --git-ignore"
-alias la="exa -la --icons --git-ignore"
+alias ls="exa --icons"
+alias la="exa -la --icons"
 alias cat="bat"
 alias fd="fd -H"
 alias tree="tree -C"
 alias gs="git st"
-alias clear="clear && colorscript -r"
 alias tree="exa -T"
 alias op="fzf | xargs nvim"
 alias cp="cp -r"
+alias clear="clear && neofetch"
 
-alias fishrc="nvim ~/.config/fish/config.fish"
-alias picomrc="nvim ~/.config/picom/picom.conf"
 alias refish="source ~/.config/fish/config.fish"
-
-alias sedit="EDITOR=nvim sudoedit"
-alias mkdirs="mkdir -p"
+alias reboot="loginctl reboot"
 
 export SUDO_EDITOR=nvim
+export XBPS_DISTDIR="/home/toxicfs/Desktop/void-packages"
 set fish_greeting
 set EDITOR nvim
-# set PATH $PATH:/home/toxicfs/.local/bin:/home/toxicfs/.cargo/bin/:/home/toxicfs/.emacs.d/bin/
 export _ZO_FZF_OPTS="--height 40"
 set CARGO_FORCE_WARN true
 set CARGO_EDITOR nvim
 set -Ux FZF_DEFAULT_OPTS "--preview 'bat {}' --color=fg:#c8ccd4,bg:#282c34,hl:#c678dd --color=fg+:#c8ccd4,bg+:#3e4451,hl+:#c678dd --color=info:#d19a66,prompt:#98c379,pointer:#e06c75 --color=marker:#e06c75,spinner:#d19a66,header:#61afef"
 if status is-interactive
+    theme.sh onedark
     clear
     zoxide init fish | source
     starship init fish | source
