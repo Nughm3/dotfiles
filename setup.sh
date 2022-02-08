0e4202854bab87bf48ec2d1ea21d0a3b47b8b1d4
Isaac Hung <manhei5001@gmail.com>
2022-02-08 21:17:45 +0800

    update void packages to fix picom

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-mkdir -p ~/.config/zathura ~/.config/fish ~/.cargo
+#mkdir -p ~/.config/zathura ~/.config/fish ~/.cargo
+mkdir -p ~/.config/zathura ~/.cargo
 cp -r bat ~/.config/
 cp -r dunst ~/.config/
 cp -r picom ~/.config/
 cp -r rofi ~/.config/
-cp starship.toml ~/.config/
-cp config.fish ~/.config/fish/
+#cp starship.toml ~/.config/
+#cp config.fish ~/.config/fish/
 cp zathurarc ~/.config/zathura/
 cp gitconfig ~/.gitconfig
 
