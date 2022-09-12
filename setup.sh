e23851d2998a156f813ad9f9a277c3af275f380d
Isaac Hung <manhei5001@gmail.com>
2022-09-12 16:18:16 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+
 mkdir -p ~/.config/zathura ~/.cargo
 cp -r bat ~/.config/
-cp -r dunst ~/.config/
 cp -r picom ~/.config/
-cp -r rofi ~/.config/
 cp zathurarc ~/.config/zathura/
 cp gitconfig ~/.gitconfig
 cp xinitrc ~/.xinitrc
-# cp zsh/* ~
 cp config.toml ~/.cargo/
+
+# cp zsh/* ~
+# cp -r rofi ~/.config/
+# cp -r dunst ~/.config/
 
 echo "Finished."
