c3862fcd2992b74f37dfd97e05adb7ece09a8b1f
Isaac Hung <manhei5001@gmail.com>
2023-01-07 11:48:21 +0800

    update

--- cargo.sh
+++ cargo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cargo install bore-cli cargo-{audit,bloat,cache,clean-recursive,clone,expand,feature,geiger,update,make,modules,nextest,outdated,release,udeps,watch,workspaces,edit} oha trunk 
+cargo install cargo-{asm,audit,bloat,cache,clean-recursive,clone,expand,feature,geiger,make,modules,nextest,outdated,release,udeps,update,watch,workspaces}
--- config.fish
+++ config.fish
@@ -1,7 +1,14 @@
+#!/bin/fish
+
 set fish_greeting
 
-export PATH="$PATH:~/.local/bin:~/.cargo/bin"
-export EDITOR="helix"
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
+
+export EDITOR="hx"
+
+alias rc="$EDITOR ~/.config/fish/config.fish"
+alias so="source ~/.config/fish/config.fish"
 
 alias e="$EDITOR"
 alias c="cargo"
@@ -12,18 +19,21 @@ alias gd="git diff"
 alias gp="git push"
 alias gu="git pull"
 alias gl="git log --oneline --graph"
-alias gg='cd $(git rev-parse --show-toplevel)'
+alias gg='cd (git rev-parse --show-toplevel)'
 
 alias ls="exa --icons"
-alias la="exa --icons --la --git"
+alias la="exa --icons -la --git"
+alias lt="exa -T --icons --git-ignore"
 
 alias cp="cp -r"
 alias mkdir="mkdir -p"
 alias md="mkdir"
 
 alias fd="fd -H"
+alias py="python3 -q"
 
 if status is-interactive
     zoxide init fish | source
     starship init fish | source
+    theme.sh onedark
 end
