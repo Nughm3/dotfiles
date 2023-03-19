01bdc1abc3147abc3078b78807a1382f591e506e
Isaac Hung <manhei5001@gmail.com>
2023-03-19 09:45:09 +0800

    update

--- misc/cargo.sh
+++ misc/cargo.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-cargo install cargo-{show-asm,audit,bloat,cache,clean-recursive,clone,expand,feature,geiger,make,modules,nextest,outdated,release,udeps,update,watch,workspaces}
+cargo install cargo-{show-asm,audit,bloat,cache,clean-recursive,clone,dist,expand,feature,geiger,make,modules,nextest,outdated,release,udeps,update,watch,workspaces}
+cargo install tauri-cli cargo-leptos trunk dioxus-cli sqlx-cli
