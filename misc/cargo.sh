a6edd950c9e78ac4ff104f0ca72bd194a35a985a
Isaac Hung <manhei5001@gmail.com>
2023-06-19 14:32:46 +0800

    remove misc

--- misc/cargo.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cargo install cargo-{show-asm,audit,bloat,cache,clean-recursive,clone,dist,expand,feature,geiger,make,modules,nextest,outdated,release,udeps,update,watch,workspaces} trunk $@
