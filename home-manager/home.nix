bb7a2ec150ae0cfee1e892a9c2d71d3c44505ca9
Isaac Hung <manhei5001@gmail.com>
2023-09-29 14:03:16 +0800

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,9 +3,196 @@
   home.homeDirectory = "/home/isaac";
 
   programs = {
+    direnv = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    eza = {
+      enable = true;
+      git = true;
+      icons = true;
+    };
+
+    fzf = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    gh = {
+      enable = true;
+      extensions = with pkgs; [ gh-cal gh-eco ];
+      settings.aliases = {
+        co = "pr checkout";
+        new = "repo create";
+        clone = "repo clone";
+      };
+    };
+
+    git = {
+      enable = true;
+      # delta.enable = true;
+      difftastic.enable = true;
+      lfs.enable = true;
+    };
+
+    gitui = {
+      enable = true;
+      theme = ''
+        (
+            selected_tab: None,
+            command_fg: None,
+            selection_bg: None,
+            selection_fg: None,
+            cmdbar_bg: None,
+            cmdbar_extra_lines_bg: None,
+            disabled_fg: None,
+            diff_line_add: None,
+            diff_line_delete: None,
+            diff_file_added: None,
+            diff_file_removed: None,
+            diff_file_moved: None,
+            diff_file_modified: None,
+            commit_hash: None,
+            commit_time: None,
+            commit_author: None,
+            danger_fg: None,
+            push_gauge_bg: None,
+            push_gauge_fg: None,
+            tag_fg: None,
+            branch_fg: None,
+        )
+      '';
+    };
+
+    starship = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    wezterm = {
+      enable = true;
+      extraConfig = ''
+        local wezterm = require("wezterm")
+        local mux = wezterm.mux
+
+        wezterm.on("gui-startup", function()
+          local tab, pane, window = mux.spawn_window{}
+          window:gui_window():maximize()
+        end)
+
+        return {
+          color_scheme = "OneDark (base16)",
+          default_prog = { "/run/current-system/sw/bin/fish" },
+
+          font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+          font_size = 10.5,
+
+          use_fancy_tab_bar = false,
+          hide_tab_bar_if_only_one_tab = true,
+  
+          window_frame = {
+            active_titlebar_bg = "#1e222a",
+            inactive_titlebar_bg = "#1e222a",
+
+            inactive_titlebar_border_bottom = "#1e222a",
+            active_titlebar_border_bottom = "#1e222a",
+    
+            button_bg = "#1e222a",
+            button_hover_bg = "#353b45",
+          },
+
+          colors = {
+            tab_bar = {
+              background = "#282c34",
+      
+              active_tab = {
+                fg_color = "#c8ccd4",
+                bg_color = "#353b45",
+
+                intensity = "Bold",
+              },
+      
+              inactive_tab = {
+                fg_color = "#abb2bf",
+                bg_color = "#282c34",
+              },
+
+              inactive_tab_hover = {
+                fg_color = "#c8ccd4",
+                bg_color = "#3e4451",
+              },
+      
+              new_tab = {
+                fg_color = "#c8ccd4",
+                bg_color = "#282c34",
+              },
+
+              new_tab_hover = {
+                fg_color = "#1e222a",
+                bg_color = "#61afef",
+              }
+            },
+          },
+
+          window_close_confirmation = "NeverPrompt"
+        }
+      '';
+    };
+
+    zathura = {
+      enable = true;
+      options = {
+        default-bg = "#282c34";
+        default-fg = "#353b45";
+        statusbar-fg = "#565c64";
+        statusbar-bg = "#3e4451";
+        inputbar-bg = "#282c34";
+        inputbar-fg = "#c8ccd4";
+        notification-bg = "#282c34";
+        notification-fg = "#c8ccd4";
+        notification-error-bg = "#282c34";
+        notification-error-fg = "#e06c75";
+        notification-warning-bg = "#282c34";
+        notification-warning-fg = "#e06c75";
+        highlight-color = "#e5c07b";
+        highlight-active-color = "#61afef";
+        completion-bg = "#353b45";
+        completion-fg = "#61afef";
+        completion-highlight-fg = "#c8ccd4";
+        completion-highlight-bg = "#61afef";
+        recolor-lightcolor = "#282c34";
+        recolor-darkcolor = "#b6bdca";
+        recolor = false;
+        recolor-keephue = false;
+      };
+    };
+
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    firefox.enable = true;
+    foot.enable = true;
     helix.enable = true;
     home-manager.enable = true;
     kakoune.enable = true;
+    mpv.enable = true;
+
+    bat.enable = true;
+    btop.enable = true;
+    gh-dash.enable = true;
+    pandoc.enable = true;
+    ripgrep.enable = true;
+    taskwarrior.enable = true;
+    tealdeer.enable = true;
+    texlive.enable = true;
+    xplr.enable = true;
+    yt-dlp.enable = true;
+    zellij.enable = true;
+
+    go.enable = true;
   };
 
   services.syncthing.enable = true;
@@ -26,55 +213,35 @@
     discord
     drawio
     figma-linux
-    firefox
     flyctl
-    foot
-    mpv
     noisetorch
     nyxt
     obsidian
     sqlitebrowser
-    wezterm
     zathura
     zotero
 
     # Command line utilities
     acpi
-    bat
     bore-cli
-    btop
     bun
     d2
-    delta
-    difftastic
-    direnv
     distrobox
     du-dust
     eva
-    eza
     fd
     fend
     ffmpeg
-    fzf
-    gitui
     glow
     imagemagick
     just
     procs
     pulsemixer
-    ripgrep
     rm-improved
     sd
-    starship
-    taskwarrior
-    tealdeer
     timewarrior
     tokei
     watchexec
-    xplr
-    yt-dlp
-    zellij
-    zoxide
 
     # Language tools
     bacon
@@ -82,7 +249,6 @@
     bun
     deno
     evcxr
-    go
     gopls
     lua
     # luajit
@@ -93,7 +259,6 @@
     nodePackages_latest.vscode-langservers-extracted
     nodePackages_latest.typescript-language-server
     nodePackages_latest.svelte-language-server
-    pandoc
     pypy3
     python3Packages.pip
     python3Packages.python-lsp-server
@@ -118,14 +283,12 @@
       ];
     }))
     texlab
-    texlive.combined.scheme-basic
     trunk
     zig
     zls
 
     # Development tools
     gdb
-    github-cli
     hyperfine
     meson
     ninja
